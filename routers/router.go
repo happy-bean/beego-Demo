aa2eca7ae71b8b3687c3d0b38b3b0b9327285d37
bean <18222997200@163.com>
2018-09-13 23:54:58 +0800

    Routing

--- routers/router.go
+++ routers/router.go
@@ -3,8 +3,22 @@ package routers
 import (
 	"beego-Demo/controllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+
+	beego.Get("/hello",func(ctx *context.Context){
+		ctx.Output.Body([]byte("hello world"))
+	})
+
+	beego.Post("/post",func(ctx *context.Context){
+		ctx.Output.Body([]byte("post"))
+	})
+
+	beego.Any("/any",func(ctx *context.Context){
+		ctx.Output.Body([]byte("any"))
+	})
+
 }
