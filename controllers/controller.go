5ab6891c49347b311ea62cd02493866d9fed327b
bean <18222997200@163.com>
2018-09-14 11:29:03 +0800

    解析json格式数据请求

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"encoding/json"
+	"beego-Demo/models"
+	"fmt"
+)
 
 type WelcomeController struct {
 	beego.Controller
@@ -18,9 +23,23 @@ type AnnotationController struct {
 
 func (c *AnnotationController) URLMapping() {
 	c.Mapping("Antest", c.Antest)
+	c.Mapping("Jsontest", c.Jsontest)
 }
 
 // @router /antest/:key [get]
 func (this *AnnotationController) Antest() {
 	this.Ctx.Output.Body([]byte("annotation controller"))
+}
+
+// @router /jsontest/:key [post]
+func (this *AnnotationController) Jsontest() {
+	var user models.User
+	println(string(this.Ctx.Input.RequestBody))
+	json.Unmarshal(this.Ctx.Input.RequestBody, &user)
+	println(user.Name)
+	jsons, errs := json.Marshal(user)
+	if errs != nil {
+		fmt.Println(errs.Error())
+	}
+	this.Ctx.Output.Body(jsons)
 }
